85bbeec1b4bac97ca5f63722983ade643f259660
Evgeniia <jonnydor@gmail.com>
2024-02-04 17:01:21 +0300

    копирование файлов

--- phonebook.py
+++ phonebook.py
@@ -18,30 +18,44 @@ def save_data(file):
     last_name = input('Введите фамилию: ')
     first_name = input('Введите имя: ')
     patronymic = input('Введите отчество: ')
-    phone_number = input('Введите номер теелфона: ')
+    phone_number = input('Введите номер телефона: ')
     with open(file, 'a', encoding='utf-8') as f:
         f.write(f'{last_name}, {first_name}, {patronymic}, {phone_number}\n')
 
 
-def search_data(contacts):
-    # ['Иван, Иванов, Иванович, 123', 'Петр, Иванов, Петрович, 456']
-    search_str = input('Введите фамилию для поиска: ')
+def search_data(contacts: list[str]): # Функция поиска данных
     founded = []
-    # search_idx
+    print('0 - поиск по фамилии')
+    print('1 - поиск по имени')
+    print('2 - поиск по отчеству')
+    print('3 - поиск по номеру телефона')
+    search_idx = input('Выберите вариант поиска: ') # для поиска по фамилии, имени, отчеству или номеру телефона
+    #search_idx для поиска по фамилии, имени, отчеству
+    search_str = input('Введите данные для поиска: ')
     for contact in contacts:
-        if search_str.lower() in contact.split(', ')[0].lower():
+        if search_str.lower() in contact.split(', ')[int(search_idx)].lower(): # разделитель - запятая, поиск по первому индексу (по фамилии)
             founded.append(contact)
     return founded
 
+def copy_data(file, file2, line):
+    with open(file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    if line > len(lines):
+        print('Нет строки с таким номером')
+        return
+    with open(file2, 'w', encoding='utf-8') as dest:
+        dest.write(f'{lines[line -1]}\n')
 
 def main():
     file_name = 'phone_book.txt'
+    file_2 = 'phones_copy.txt'
     flag = True
     while flag:
         print('0 - выход')
         print('1 - запись в файл')
         print('2 - показать записи')
         print('3 - найти запись')
+        print('4 - скопировать данные')
         answer = input('Выберите действие: ')
         if answer == '0':
             flag = False
@@ -54,6 +68,10 @@ def main():
             data = read_file(file_name)
             founded_data = search_data(data)
             show_data(founded_data)
+        elif answer == '4':
+            line_number = int(input('Введите номер строки, которую нужно скопировать в другой файл: '))
+            copy_data(file_name, file_2, line_number)
+
 
 if __name__ == '__main__':
     main()
